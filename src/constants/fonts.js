7689502db5150321658c19ef94e48dd9158a7574
diyorbek-yusupov <dyuzb2004@gmail.com>
2023-09-06 23:37:56 +0500

    [add] optimized font added

---
+++ src/constants/fonts.js
@@ -0,0 +1,109 @@
+import localFont from "next/font/local";
+
+export const atypDisplay = localFont({
+  variable: "--font-atypDisplay",
+  src: [
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-Bold.woff2",
+      weight: "700", // Use "700" for bold
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-BoldItalic.woff2",
+      weight: "700", // Use "700" for bold
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-SemiBold.woff2",
+      weight: "600", // Use "600" for semibold
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-SemiBoldItalic.woff2",
+      weight: "600", // Use "600" for semibold
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-Medium.woff2",
+      weight: "500", // Use "500" for medium
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-MediumItalic.woff2",
+      weight: "500", // Use "500" for medium
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-Light.woff2",
+      weight: "300", // Use "300" for light
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-Regular.woff2",
+      weight: "400", // Use "400" for regular
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-Italic.woff2",
+      weight: "400", // Use "400" for regular
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-LightItalic.woff2",
+      weight: "300", // Use "300" for light
+      style: "italic",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-Thin.woff2",
+      weight: "100", // Use "100" for thin
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/AtypDisplay/AtypDisplay-ThinItalic.woff2",
+      weight: "100", // Use "100" for thin
+      style: "italic",
+    },
+  ],
+});
+
+export const hauora = localFont({
+  variable: "--font-hauora",
+
+  src: [
+    {
+      path: "../assets/fonts/Hauora/Hauora-Bold.woff2",
+      weight: "700", // Bold
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Hauora/Hauora-ExtraBold.woff2",
+      weight: "800", // Extra Bold
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Hauora/Hauora-ExtraLight.woff2",
+      weight: "200", // Extra Light
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Hauora/Hauora-Light.woff2",
+      weight: "300", // Light
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Hauora/Hauora-Medium.woff2",
+      weight: "500", // Medium
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Hauora/Hauora-Regular.woff2",
+      weight: "400", // Regular
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Hauora/Hauora-SemiBold.woff2",
+      weight: "600", // SemiBold
+      style: "normal",
+    },
+  ],
+});
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import { appWithTranslation } from "next-i18next";
 import Layout from "@/layout/Layout";
 import "@/styles/globals.css";
+import { atypDisplay, hauora } from "@/constants/fonts";
 
 function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={`${atypDisplay.variable} ${hauora.variable}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
 
